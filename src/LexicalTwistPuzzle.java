f8c72a2c10d345859d060ec8ad6359a29556d020
Wave-Shot <vsh.2509@gmail.com>
2026-02-13 11:32:21 +0530

    [Srihari]: UC 5 - Transform Word if Reverse Match

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -2,19 +2,17 @@ import java.util.Scanner;
 
 public class LexicalTwistPuzzle {
     public static void main(String[] args) {
-        System.out.println("Lexical Twist Puzzle");
         Scanner sc = new Scanner(System.in);
         String word1 = sc.nextLine();
         String word2 = sc.nextLine();
         if (word1.contains(" ") || word2.contains(" ")) {
-            System.out.println("Invalid input");
             return;
         }
         String reversed = new StringBuilder(word1).reverse().toString();
         if (reversed.equalsIgnoreCase(word2)) {
-            System.out.println("Reverse match found");
-        } else {
-            System.out.println("No reverse match");
+            String lower = reversed.toLowerCase();
+            String transformed = lower.replaceAll("[aeiou]", "@");
+            System.out.println(transformed);
         }
     }
 }
